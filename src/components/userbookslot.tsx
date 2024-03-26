fc92af2cb3b32ccfd9f14e05f97e2b25e62a9daf
RawSalmon <phanthawasjira@Gmail.com>
2024-03-27 00:02:49 +0700

    removed unused components

--- src/components/userbookslot.tsx
+++ src/components/userbookslot.tsx
@@ -1,37 +1,37 @@
-"use client"
-import getadminbookslot from "@/libs/getadminbookslot";
+// "use client"
+// import getadminbookslot from "@/libs/getadminbookslot";
 
-import { AppDispatch, useAppSelector } from "@/redux/store"
-import { useDispatch } from "react-redux"
-import { removeReservation } from "@/redux/features/cartSlice"
+// import { AppDispatch, useAppSelector } from "@/redux/store"
+// import { useDispatch } from "react-redux"
+// import { removeReservation } from "@/redux/features/cartSlice"
 
-export default function userbookslot(){
+// export default function userbookslot(){
 
-    const bookslots = getadminbookslot()
+//     const bookslots = getadminbookslot()
 
-    const carItems = useAppSelector((state)=>state.cartSlice.carItems)
-    const dispatch = useDispatch<AppDispatch>()
+//     const carItems = useAppSelector((state)=>state.cartSlice.carItems)
+//     const dispatch = useDispatch<AppDispatch>()
 
-    return(
-        <>
-        {
-            carItems.map((reservationItem)=>(
-                <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2"
-                key={reservationItem.carID}>
-                    <div className="text-xl">{reservationItem.user}</div>
-                    <div className="text-sm">Pick-up{reservationItem.apptDate}
-                    from {reservationItem.pickupLocation}</div>
-                    <div className="text-sm">return{reservationItem.company}
-                    to {reservationItem.returnLocation}</div>
-                    <div className="text-md" >Duration: {reservationItem.createdAt}</div>
-                    <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
-                    onClick={()=>dispatch(removeReservation(reservationItem))}>
-                        Remove from Cart
-                    </button>
-                </div>
+//     return(
+//         <>
+//         {
+//             carItems.map((reservationItem)=>(
+//                 <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2"
+//                 key={reservationItem.carID}>
+//                     <div className="text-xl">{reservationItem.user}</div>
+//                     <div className="text-sm">Pick-up{reservationItem.apptDate}
+//                     from {reservationItem.pickupLocation}</div>
+//                     <div className="text-sm">return{reservationItem.company}
+//                     to {reservationItem.returnLocation}</div>
+//                     <div className="text-md" >Duration: {reservationItem.createdAt}</div>
+//                     <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
+//                     onClick={()=>dispatch(removeReservation(reservationItem))}>
+//                         Remove from Cart
+//                     </button>
+//                 </div>
                 
                 
-            ))
-        }</>
-    )
-}
+//             ))
+//         }</>
+//     )
+// }
--- src/libs/getadminAppts.tsx
+++ src/libs/getadminAppts.tsx
@@ -1,13 +1,13 @@
-import { resolve } from "path"
+// import { resolve } from "path"
 
-export default async function getadminAppts() {
+// export default async function getadminAppts() {
 
-    await new Promise((resolve)=>setTimeout(resolve,5000))
+//     await new Promise((resolve)=>setTimeout(resolve,5000))
 
-    const response = await fetch("http://localhost:5000/api/v1/appointments")
-    if(!response.ok){
-        throw new Error("Failed to fetch appointments")
-    }
+//     const response = await fetch("http://localhost:5000/api/v1/appointments")
+//     if(!response.ok){
+//         throw new Error("Failed to fetch appointments")
+//     }
 
-    return await response.json()
-}
+//     return await response.json()
+// }
