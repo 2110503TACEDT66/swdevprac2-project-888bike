2741f19e6219b207519f5bf9bd9754b78a1c73a6
RawSalmon <phanthawasjira@Gmail.com>
2024-03-26 16:13:37 +0700

    Added Appointment Page

--- src/app/booking/user/page.tsx
+++ src/app/booking/user/page.tsx
@@ -11,15 +11,16 @@ import { addReservation } from "@/redux/features/cartSlice";
 export default function Reservations () {
 
     const urlParams = useSearchParams()
-    const cid = urlParams.get('id')
-    const model = urlParams.get('model')
+    const company = urlParams.get('company')
+    const cid = urlParams.get('cid')
 
     // const dispatch = useDispatch<AppDispatch>()
+    const [apptTime, setApptTime] = useState<string>('');
 
-    const [pickupDate,setPickupDate] = useState<Dayjs|null>()
-    const [pickupLocation,setPickupLocation] = useState<string>('BKK')
-    const [returnDate,setReturnDate] = useState<Dayjs|null>()
-    const [returnLocation,setReturnLocation] = useState<string>('BKK')
+    const setAppointmentTime = (value:string) =>{
+        console.log(value)
+        setApptTime(value)
+    }
 
     // const makeReservation = ()=> {
     //     if(cid&&model&&pickupDate&&returnDate){
@@ -38,13 +39,12 @@ export default function Reservations () {
 
     return (
         <main className="w-[100%] flex flex-col items-center space-y-4">
-            <div className="text-xl font-medium">New Reservation</div>
-            <div className="text-xl font-medium">Car : {model}</div>
+            <div className="text-2xl font-semibold mt-3">New Appointment</div>
+            <div className="text-xl font-medium">Company : {company}</div>
 
             <div className="w-fit space-y-2">
                 <div className="text-md text-left text-gray-600">Pick-Up Date Time and Company</div>
-                <LocationDateReserve onDateChange={(value:Dayjs)=>{setPickupDate(value)}} 
-                onLocationChange={(value:string)=>{setPickupLocation(value)}} />
+                <LocationDateReserve onDateTimeChange={(value: string)=>{setAppointmentTime(value)}}/>
             </div>
 
             <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
--- src/components/LocationDateReserve.tsx
+++ src/components/LocationDateReserve.tsx
@@ -10,40 +10,53 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Select, MenuItem, TextField } from "@mui/material";
 import dayjs, { Dayjs } from "dayjs";
 
-export default function LocationDateAndTimeReserve({ onDateTimeChange, onLocationChange }:
-    { onDateTimeChange: (date: Dayjs | null, time: Dayjs | null) => void, onLocationChange: Function }) {
+export default function LocationDateAndTimeReserve({onDateTimeChange}: {onDateTimeChange: Function}) {
 
-    const [reserveDate, setReserveDate] = useState<Dayjs | null>(null);
-    const [reserveTime, setReserveTime] = useState<Dayjs | null>(null);
-    const [location, setLocation] = useState('BKK');
+    const [reserveDate, setReserveDate] = useState<string>('');
+    const [reserveTime, setReserveTime] = useState<string>('');
 
     // Handle changing both date and time
     const handleDateTimeChange = (newValue: Dayjs | null, type: 'date' | 'time') => {
         if (type === 'date') {
-            setReserveDate(newValue);
+            // setReserveDate(newValue.toISOString());
+            const newDate = newValue?.toISOString();
+            setReserveDate(newDate);
+            // console.log('newValue', newValue?.toISOString())
+            console.log('reserveDate', reserveDate);
         } else {
-            setReserveTime(newValue);
+            // setReserveTime(newValue.toISOString());
+            const newTime = newValue?.toISOString();
+            setReserveTime(newTime);
+            console.log('reserveTime', reserveTime);
         }
-        onDateTimeChange(reserveDate, reserveTime);
+        if(reserveDate != '' && reserveTime != ''){
+            const newDate = reserveDate.split('T')[0];
+            const newTime = reserveTime.split('T')[1];
+            const newDateTime = newDate + 'T' + newTime;
+            console.log('newDateTime', newDateTime);
+            onDateTimeChange(newDateTime);
+        }
+        
     };
 
     return (
         <div className="bg-slate-100 rounded-lg space-x-5 space-y-2 w-fit px-10 py-5 flex flex-row justify-center">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
+                
                 <DatePicker
-                    className="bg-white"
-                    value={reserveDate}
+                className="mt-2"
+                    // value={reserveDate}
                     onChange={(newValue) => handleDateTimeChange(newValue, 'date')}
                     renderInput={(params) => <TextField {...params} />}
                 />
                 <TimePicker
-                    value={reserveTime}
+                    // value={reserveTime}
                     onChange={(newValue) => handleDateTimeChange(newValue, 'time')}
                     renderInput={(params) => <TextField {...params} />}
                 />
             </LocalizationProvider>
 
-            <Select
+            {/* <Select
                 variant="standard"
                 name="location"
                 id="location"
@@ -54,7 +67,7 @@ export default function LocationDateAndTimeReserve({ onDateTimeChange, onLocatio
                 <MenuItem value="BKK">Bangkok</MenuItem>
                 <MenuItem value="CNX">Chiang Mai</MenuItem>
                 <MenuItem value="HKT">Phuket</MenuItem>
-            </Select>
+            </Select> */}
         </div>
     );
 }
